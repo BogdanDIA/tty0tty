866ce0d40ac1da39d4f4618f0a7d97d21fa881c1
BogdanDIA <b_diaconescu@yahoo.com>
2024-08-23 22:41:21 +0300

    Changed definition of write and set_termios funcions

--- module/tty0tty.c
+++ module/tty0tty.c
@@ -210,8 +210,8 @@ static void tty0tty_close(struct tty_struct *tty, struct file *file)
 		do_close(tty0tty);
 }
 
-static int tty0tty_write(struct tty_struct *tty, const unsigned char *buffer,
-			 int count)
+static ssize_t tty0tty_write(struct tty_struct *tty, const unsigned char *buffer,
+			 size_t count)
 {
 	struct tty0tty_serial *tty0tty = tty->driver_data;
 	int retval = 0;
@@ -288,7 +288,7 @@ exit:
 #define RELEVANT_IFLAG(iflag) ((iflag) & (IGNBRK|BRKINT|IGNPAR|PARMRK|INPCK))
 
 static void tty0tty_set_termios(struct tty_struct *tty,
-				struct ktermios *old_termios)
+				const struct ktermios *old_termios)
 {
 	unsigned int cflag;
 	unsigned int iflag;
